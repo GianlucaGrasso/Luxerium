a42f48af876cf4369fd81fc8a8ad31cd102b9069
Gianluca Danilo Grasso <gianlucadanilograsso@gmail.com>
2024-09-16 01:10:40 -0300

    Major changes

--- luxerium-app/tailwind.config.mjs
+++ luxerium-app/tailwind.config.mjs
@@ -5,6 +5,21 @@ export default {
 		extend: {
 			backgroundImage: {
 				'feature': "url('https://placehold.co/1920x500')",
+			  },
+
+			  fontFamily: {
+				primaryFont: ['Genera Grotesk', 'sans-serif'],
+				secondaryFont: ['Archivo', 'sans-serif'],
+				// Add more custom font families as needed
+			  },
+			  colors: {
+				'primaryColor': '#F6F6F6',
+				'secondaryColor': '#F99929',
+				'terciaryColor': '#252525',
+				'Red': '#874545',
+				'Green': '#45874C',
+				'lightGreyFade': 'rgba(9, 9, 9, 0.71)',
+				// Configure your color palette here
 			  }
 		},
 	},
