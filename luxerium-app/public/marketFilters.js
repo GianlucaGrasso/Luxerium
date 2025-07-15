5d5fe6242c04752f2665abebb4b11710c78fab16
Gianluca Grasso <gianlucadanilograsso@gmail.com>
2025-07-15 04:36:43 -0300

    Major Changes

---
+++ luxerium-app/public/marketFilters.js
@@ -0,0 +1,388 @@
+
+export const MarketFilters = {
+
+    realEstate: {
+        title: "Real Estate",
+        image: "./images/market/RealEstate.webp",
+        types: {
+            housesTypes: {
+                title: "Houses",
+                image: "./images/market/Houses/Houses.webp",
+                types: {
+                    mansions: {
+                        title: "Mansions",
+                        image: "./images/market/Houses/Mansions.webp",
+                    },
+                    townhouses: {
+                        title: "Townhouses",
+                        image: "./images/market/Houses/Townhouses.webp",
+                    },
+                    villas: {
+                        title: "Villas",
+                        image: "./images/market/Houses/Villas.webp",
+                    },
+                    cottages: {
+                        title: "Cottages",
+                        image: "./images/market/Houses/Cottages.webp",
+                    },
+                    ranchs: {
+                        title: "Ranchs",
+                        image: "./images/market/Houses/Ranchs.webp",
+                    },
+                    cabins: {
+                        title: "Cabins",
+                        image: "./images/market/Houses/Cabins.webp",
+                    },
+                }
+            },
+            apartmentsTypes: {
+                title: "Apartments",
+                image: "./images/market/Apartments/Apartments.webp",
+                types: {
+                    studios: {
+                        title: "Studios",
+                        image: "./images/market/Apartments/Studios.webp",
+                    },
+                    penthouses: {
+                        title: "Penthouses",
+                        image: "./images/market/Apartments/Penthouses.webp",
+                    },
+                    lofts: {
+                        title: "Lofts",
+                        image: "./images/market/Apartments/Lofts.webp",
+                    },
+                    condos: {
+                        title: "Condos",
+                        image: "./images/market/Apartments/Condos.webp",
+                    },
+                }
+            },
+            buildingsTypes: {
+                title: "Buildings",
+                image: "./images/market/Buildings/Buildings.webp",
+                types: {
+                    offices: {
+                        title: "Offices",
+                        image: "./images/market/Buildings/Offices.webp",
+                    },
+                    warehouses: {
+                        title: "Warehouses",
+                        image: "./images/market/Buildings/Warehouses.webp",
+                    },
+                    hotels: {
+                        title: "Hotels",
+                        image: "./images/market/Buildings/Hotels.webp",
+                    },
+                }
+            },
+            landsTypes: {
+                title: "Lands",
+                image: "./images/market/Lands/Lands.webp",
+                types: {
+                    residential: {
+                        title: "Residential",
+                        image: "./images/market/Lands/Residential.webp",
+                    },
+                    commercial: {
+                        title: "Commercial",
+                        image: "./images/market/Lands/Commercial.webp",
+                    },
+                    agricultural: {
+                        title: "Agricultural",
+                        image: "./images/market/Lands/Agricultural.webp",
+                    },
+                    industrial: {
+                        title: "Industrial",
+                        image: "./images/market/Lands/Industrial.webp",
+                    },
+                },
+            },
+        },
+    },
+
+    vehicles: {
+        title: "Vehicles",
+        image: "./images/market/Vehicles.webp",
+        types: {
+            carsTypes: {
+                title: "Cars",
+                image: "./images/market/Cars/Cars.webp",
+                types: {
+                    sportsCar: {
+                        title: "Sports Cars",
+                        image: "./images/market/Cars/SportsCars.webp",
+                    },
+                    suvs: {
+                        title: "Suvs",
+                        image: "./images/market/Cars/Suvs.webp",
+                    },
+                    sedans: {
+                        title: "Sedans",
+                        image: "./images/market/Cars/Sedans.webp",
+                    },
+                    hatchbacks: {
+                        title: "Hatchbacks",
+                        image: "./images/market/Cars/Hatchbacks.webp",
+                    },
+                    coupes: {
+                        title: "Coupes",
+                        image: "./images/market/Cars/Coupes.webp",
+                    },
+                    convertibles: {
+                        title: "Convertibles",
+                        image: "./images/market/Cars/Convertibles.webp",
+                    },
+                    pickups: {
+                        title: "Pickups",
+                        image: "./images/market/Cars/Pickups.webp",
+                    },
+                    vans: {
+                        title: "Vans",
+                        image: "./images/market/Cars/Vans.webp",
+                    },
+                }
+
+            },
+            motorcycleTypes: {
+                title: "Motorcycles",
+                image: "./images/market/Motorcycles/Motorcycles.webp",
+                types: {
+                    standard: {
+                        title: "Standard",
+                        image: "./images/market/Motorcycles/Standard.webp",
+                    },
+                    sportBikes: {
+                        title: "Sport Bikes",
+                        image: "./images/market/Motorcycles/SportBikes.webp",
+                    },
+                    cruisers: {
+                        title: "Cruisers",
+                        image: "./images/market/Motorcycles/Cruisers.webp",
+                    },
+                    touringBikes: {
+                        title: "Touring Bikes",
+                        image: "./../images/market/Motorcycles/TouringBikes.webp",
+                    },
+                    chopper: {
+                        title: "Chopper",
+                        image: "./images/market/Motorcycles/Chopper.webp",
+                    },
+                    scooter: {
+                        title: "Scooter",
+                        image: "./images/market/Motorcycles/Scooter.webp",
+                    },
+                },
+            },
+            truckTypes: {
+                title: "Trucks",
+                image: "./images/market/Trucks/Trucks.webp",
+                types: {
+                    semiTrucks: {
+                        title: "Semi Trucks",
+                        image: "./images/market/Trucks/SemiTrucks.webp",
+                    },
+                    livestockTrucks: {
+                        title: "Livestock Trucks",
+                        image: "./images/market/Trucks/LivestockTrucks.webp",
+                    },
+                    smallTrucks: {
+                        title: "Small Trucks",
+                        image: "./images/market/Trucks/SmallTrucks.webp",
+                    },
+                    boxTrucks: {
+                        title: "Box Trucks",
+                        image: "./images/market/Trucks/BoxTrucks.webp",
+                    },
+                }
+            },
+            offroadTypes: {
+                title: "Offroad Vehicles",
+                image: "./images/market/Offroad/Offroad.webp",
+                types: {
+                    utilityAtvs: {
+                        title: "Utility ATVs",
+                        image: "./images/market/Offroad/UtilityATVs.webp",
+                    },
+                    snowmobiles: {
+                        title: "Snowmobiles",
+                        image: "./images/market/Offroad/Snowmobiles.webp",
+                    },
+                    offRoadBikes: {
+                        title: "Off-Road Bikes",
+                        image: "./images/market/Offroad/OffRoadBikes.webp",
+                    },
+                    taskVehicles: {
+                        title: "Task Vehicles",
+                        image: "./images/market/Offroad/TaskVehicles.webp",
+                    },
+                }
+            }
+        },
+            
+    },
+
+    aircrafts: {
+        title: "Aircrafts",
+        image: "./images/market/Aircrafts.webp",
+        types: {
+            privateJets: {
+                title: "Private Jets",
+                image: "./images/market/Aircrafts/PrivateJets.webp",
+            },
+            commercialAircrafts: {
+                title: "Commercial Aircrafts",
+                image: "./images/market/Aircrafts/CommercialAircrafts.webp",
+            },
+            helicopters: {
+                title: "Helicopters",
+                image: "./images/market/Aircrafts/Helicopters.webp",
+            },
+            gliders: {
+                title: "Gliders",
+                image: "./images/market/Aircrafts/Gliders.webp",
+            },
+            seaplanes: {
+                title: "Seaplanes",
+                image: "./images/market/Aircrafts/Seaplanes.webp",
+            },
+            evtols: {
+                title: "EVTOLs",
+                image: "./images/market/Aircrafts/EVTOLs.webp",
+            },
+        }
+    },
+
+    ships: {
+        title: "Ships",
+        image: "./images/market/Ships.webp",
+        types: {
+            yatchs: {
+                title: "Yachts",
+                image: "./images/market/Ships/Yachts.webp",
+            },
+            catamarans: {
+                title: "Catamarans",
+                image: "./images/market/Ships/Catamarans.webp",
+            },
+            sailboats: {
+                title: "Sailboats",
+                image: "./images/market/Ships/Sailboats.webp",
+            },
+            boats: {
+                title: "Boats",
+                image: "./images/market/Ships/Boats.webp",
+            },
+            cruisers: {
+                title: "Cruisers",
+                image: "./../images/market/Ships/CruiseShips.webp",
+            },
+            jetskis: {
+                title: "Jet Skis",
+                image: "./images/market/Ships/JetSkis.webp",
+            },
+        }
+    }
+    
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+/*
+    export const realEstateTypes = { 
+    1: "HOUSES",
+    2: "APARTMENTS",
+    3: "BUILDINGS",
+    4: "LANDS",
+}
+    export const houseTypes = {
+    1: "MANSIONS",
+    2: "TOWNHOUSES",
+    3: "VILLAS",
+    4: "COTTAGES",
+    5: "RANCHS",
+    6: "CABINS",
+}
+    export const apartmentTypes = { 
+    1: "STUDIOS",
+    2: "PENTHOUSES",
+    3: "LOFTS",
+    4: "CONDOS",
+}
+    export const buildingTypes = { 
+    1: "OFFICES",
+    2: "WAREHOUSES",
+    3: "HOTELS",
+}
+    export const landTypes = { 
+    1: "RESIDENTIAL",
+    2: "COMMERCIAL",
+    3: "AGRICULTURAL",
+    4: "INDUSTRIAL",
+}
+
+
+export const vehicleTypes = { 
+   1: "CARS",
+   2: "MOTORCYCLES",
+   3: "TRUCKS",
+   4: "OFFROAD",
+}
+
+export const carTypes = {
+    1: "SPORTS CARS",
+    2: "SUVS",
+    3: "SEDANS",
+    4: "HATCHBACKS",
+    5: "COUPES",
+    6: "CONVERTIBLES",
+    7: "PICKUPS",
+    8: "VANS",
+    9: "SUPERCARS",
+}
+
+export const motorcycleTypes = { 
+    1: "STANDARD",
+    2: "SPORT BIKES",
+    3: "CRUISERS",
+    4: "TOURING BIKES",
+    5: "CHOPPER",
+    6: "SCOOTER",
+}
+
+export const truckTypes = { 
+    1: "SEMI TRUCKS",
+    2: "LIVESTOCK TRUCKS",
+    3: "SMALL TRUCKS",
+    4: "BOX TRUCKS",
+}
+
+export const offroadTypes = { 
+    1: "UTILITY ATVS",
+    2: "SNOWMOBILES",
+    3: "OFF-ROAD BIKES",
+    4: "TASK VEHICLES",
+}
+
+// AIRCRAFTS FILTER
+
+export const aircraftTypes = { 
+   1: "PRIVATE JETS",
+   2: "COMMERCIAL AIRCRAFTS",
+   3: "HELICOPTERS",
+   4: "GLIDERS",
+   5: "SEAPLANES",
+   6: "EVTOLS",
+}
+
+// SHIPS FILTER
+
+export const shipTypes = { 
+   1: "YACHTS",
+   2: "CATAMARANS",
+   3: "SAILBOATS",
+   4: "BOATS",
+   5: "CRUISERS",
+   6: "JETSKIS"
+}
+*/
---
+++ luxerium-app/src/filter.jsx
@@ -0,0 +1,22 @@
+import { MarketFilters } from "../public/marketFilters";
+
+
+function Filter() {
+
+    
+}
+
+
+/*
+import React from "react";
+import BrowseBy from "./components/subComponents/BrowseBy.astro";
+
+
+const Filter = () => {
+    return (
+        <BrowseBy title={realEstateTypes[1]} size="sm" image="https://images.unsplash.com/photo-1721902474248-3d6d2f2b595e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    );
+}
+
+export default Filter;
+*/
